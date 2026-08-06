c8d78475a075a9ca3c59f8edf479b3898f6fa755
Shweta Chawla <shwetachawla023@gmail.com>
2026-08-06 17:52:54 +0530

    testing 1

---
+++ azure_landing_zone/child_module/azurerm_key_vault/data.tf
@@ -0,0 +1,14 @@
+data "azurerm_key_vault" "example" {
+  for_each = var.key_vault
+
+   name                = each.value.key_vault_name
+  resource_group_name = each.value.resource_group_name
+}
+
+data "azurerm_key_vault_secret" "secret" {
+  for_each = var.key_vault  
+  
+  name         = each.value.secret_name
+  key_vault_id = data.azurerm_key_vault.example[each.key].id
+}
+
